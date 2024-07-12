1441eaa5c3b1b8c58cc2bde710323fadfc21c04a
denis12676 <denis12676@gmail.com>
2024-07-13 02:19:06 +0300

    ran server

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!\n"))
+}
 func main() {
-	fmt.Println("你好, 世界")
+	http.HandleFunc("/", Handler)
+
+	log.Println("Starting server...")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
